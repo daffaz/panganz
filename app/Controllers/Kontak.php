1ea188929d2c36633fd87ab6fec365883bc1619b
Daffa Z <67418594+j3c118124@users.noreply.github.com>
2020-12-05 13:45:42 +0700

    Update Kontak.php

--- app/Controllers/Kontak.php
+++ app/Controllers/Kontak.php
@@ -7,7 +7,7 @@ class Kontak extends BaseController
     public function index()
     {
         echo view('header_v');
-        echo view('header_v');
-        echo view('header_v');
+        echo view('kontak_v');
+        echo view('footer_v');
     }
 }
