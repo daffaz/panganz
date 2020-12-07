840c1e6558f027608e995ce63a7f7a802678dbf5
Daffa Z <67418594+j3c118124@users.noreply.github.com>
2020-12-08 01:00:01 +0700

    Create ArtikelTrendingModel.php

---
+++ app/Models/ArtikelTrendingModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ArtikelTrendingModel extends Model
+{
+    protected $table      = 'tbl_artikel_trending';
+    protected $primaryKey = 'id';
+
+    protected $returnType     = 'object';
+    protected $useSoftDeletes = false;
+
+    protected $allowedFields = ['judul', 'organisasi', 'nama', 'tanggal', 'berita', 'gambar', 'berita2'];
+
+    protected $useTimestamps = false;
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    protected $validationRules    = [];
+    protected $validationMessages = [];
+    protected $skipValidation     = false;
+}
