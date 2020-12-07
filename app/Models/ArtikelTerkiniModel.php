37af7f27cf661d76b29cb6d41c1e28a38e116708
Daffa Z <67418594+j3c118124@users.noreply.github.com>
2020-12-08 00:59:58 +0700

    Create ArtikelTerkiniModel.php

---
+++ app/Models/ArtikelTerkiniModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ArtikelTerkiniModel extends Model
+{
+    protected $table      = 'tbl_artikel_terkini';
+    protected $primaryKey = 'id';
+
+    protected $returnType     = 'object';
+    protected $useSoftDeletes = false;
+
+    protected $allowedFields = ['judul', 'organisasi', 'nama', 'tanggal', 'berita', 'gambar', 'berita2'];
+
+    protected $useTimestamps = false;
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    protected $validationRules    = [];
+    protected $validationMessages = [];
+    protected $skipValidation     = false;
+}
