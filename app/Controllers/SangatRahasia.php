f064d4c797184f2dfe444535fb581a1f4b1f8fe6
Daffa Z <67418594+j3c118124@users.noreply.github.com>
2020-12-20 23:52:39 +0700

    Update SangatRahasia.php

--- app/Controllers/SangatRahasia.php
+++ app/Controllers/SangatRahasia.php
@@ -2,13 +2,15 @@
 
 namespace App\Controllers;
 
-use App\Models\AdminAcaraModel;
+use App\Models\AcaraModel;
+use App\Models\ArtikelTrendingModel;
 
 class SangatRahasia extends BaseController
 {
     public function __construct()
     {
-        $this->adminAcaraModel = new AdminAcaraModel();
+        $this->adminAcaraModel = new AcaraModel();
+        $this->adminArtikelTrendingModel = new ArtikelTrendingModel();
     }
     public function index()
     {
@@ -21,10 +23,21 @@ class SangatRahasia extends BaseController
     public function adminAcara()
     {
         $data['dataAcara'] = $this->adminAcaraModel->findAll();
+
         echo view('admin/admin_header_v');
         echo view('admin/admin_sidebar_v');
         echo view('admin/admin_topbar_v');
         echo view('admin/acara/admin_acara_v', $data);
+        echo view('admin/admin_footer_v.php');
+    }
+    public function adminArtikelTrending()
+    {
+        $data['dataTrending'] = $this->adminArtikelTrendingModel->findAll();
+
+        echo view('admin/admin_header_v');
+        echo view('admin/admin_sidebar_v');
+        echo view('admin/admin_topbar_v');
+        echo view('admin/artikel/admin_artikel_trending_v', $data);
         echo view('admin/admin_footer_v.php');
     }
 }
