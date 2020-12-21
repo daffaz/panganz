9368b50b10562e97dd9eb6a0718fe5f82d542f69
Daffa Z <dzcoc123@gmail.com>
2020-12-21 21:17:57 +0700

    Update admin_artikel_lawas_v.php

---
+++ app/Views/admin/artikel/admin_artikel_lawas_v.php
@@ -0,0 +1,32 @@
+<section>
+    <div class="card">
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Judul</th>
+                        <th scope="col">Organisasi</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">Tanggal</th>
+                        <th scope="col">Berita</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $nomor = 1 ?>
+                    <?php foreach ($dataLawas as $d) : ?>
+                        <tr>
+                            <th scope="row"><?= $nomor ?></th>
+                            <td><?= $d->judul ?></td>
+                            <td><?= $d->organisasi ?></td>
+                            <td><?= $d->nama ?></td>
+                            <td><?= $d->tanggal ?></td>
+                            <td><?= $d->berita . '... ' ?></td>
+                        </tr>
+                        <?php $nomor++ ?>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</section>
