62d9ed6c98168f84dff8a14ca40daf24d61dfb5c
Daffa Z <dzcoc123@gmail.com>
2020-12-21 21:18:04 +0700

    Update admin_sidebar_v.php

--- app/Views/admin/admin_sidebar_v.php
+++ app/Views/admin/admin_sidebar_v.php
@@ -2,7 +2,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?=base_url('SangatRahasia')?>">
         <h1 class="font-weight-bold">P</h1>
         <div class="sidebar-brand-text">anganZ Admin</div>
     </a>
@@ -12,7 +12,7 @@
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item">
-        <a class="nav-link" href="index.html">
+      <a class="nav-link" href="<?=base_url('SangatRahasia')?>">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
     </li>
@@ -29,7 +29,19 @@
     <li class="nav-item">
         <a class="nav-link collapsed" href="<?= base_url('SangatRahasia/adminArtikelTrending') ?>">
             <i class="far fa-newspaper"></i>
-            <span>Manage articles</span>
+            <span>Manage artikel trending</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="<?= base_url('SangatRahasia/adminArtikelTerkini') ?>">
+            <i class="far fa-newspaper"></i>
+            <span>Manage artikel terkini</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="<?= base_url('SangatRahasia/adminArtikelLawas') ?>">
+            <i class="far fa-newspaper"></i>
+            <span>Manage artikel lawas</span>
         </a>
     </li>
 
