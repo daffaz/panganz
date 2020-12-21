7d8acc4f6c2ec3a593806c04bfd51d33b79e3258
Daffa Z <dzcoc123@gmail.com>
2020-12-21 21:18:20 +0700

    Update SangatRahasia.php

--- app/Controllers/SangatRahasia.php
+++ app/Controllers/SangatRahasia.php
@@ -4,6 +4,8 @@ namespace App\Controllers;
 
 use App\Models\AcaraModel;
 use App\Models\ArtikelTrendingModel;
+use App\Models\ArtikelTerkiniModel;
+use App\Models\ArtikelLawasModel;
 
 class SangatRahasia extends BaseController
 {
@@ -11,6 +13,8 @@ class SangatRahasia extends BaseController
     {
         $this->adminAcaraModel = new AcaraModel();
         $this->adminArtikelTrendingModel = new ArtikelTrendingModel();
+        $this->adminArtikelTerkiniModel = new ArtikelTerkiniModel();
+        $this->adminArtikelLawasModel = new ArtikelLawasModel();
     }
     public function index()
     {
@@ -23,9 +27,10 @@ class SangatRahasia extends BaseController
     public function adminAcara()
     {
         $data['dataAcara'] = $this->adminAcaraModel->findAll();
+        $test['judul'] = "Acara";
 
         echo view('admin/admin_header_v');
-        echo view('admin/admin_sidebar_v');
+        echo view('admin/admin_sidebar_v', $test);
         echo view('admin/admin_topbar_v');
         echo view('admin/acara/admin_acara_v', $data);
         echo view('admin/admin_footer_v.php');
@@ -33,11 +38,34 @@ class SangatRahasia extends BaseController
     public function adminArtikelTrending()
     {
         $data['dataTrending'] = $this->adminArtikelTrendingModel->findAll();
+        $test['judul'] = "Artikel Trending";
 
         echo view('admin/admin_header_v');
         echo view('admin/admin_sidebar_v');
-        echo view('admin/admin_topbar_v');
+        echo view('admin/admin_topbar_v', $test);
         echo view('admin/artikel/admin_artikel_trending_v', $data);
+        echo view('admin/admin_footer_v.php');
+    }
+    public function adminArtikelTerkini()
+    {
+        $data['dataTerkini'] = $this->adminArtikelTerkiniModel->findAll();
+        $test['judul'] = "Artikel Terkini";
+
+        echo view('admin/admin_header_v');
+        echo view('admin/admin_sidebar_v');
+        echo view('admin/admin_topbar_v', $test);
+        echo view('admin/artikel/admin_artikel_terkini_v', $data);
+        echo view('admin/admin_footer_v.php');
+    }
+    public function adminArtikelLawas()
+    {
+        $data['dataLawas'] = $this->adminArtikelLawasModel->findAll();
+        $test['judul'] = "Artikel Lawas";
+
+        echo view('admin/admin_header_v');
+        echo view('admin/admin_sidebar_v');
+        echo view('admin/admin_topbar_v', $test);
+        echo view('admin/artikel/admin_artikel_lawas_v', $data);
         echo view('admin/admin_footer_v.php');
     }
 }
