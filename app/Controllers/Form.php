cd13871f36f5b2dc16eb80ded3bb63b0d0b4e679
Daffa Z <dzcoc123@gmail.com>
2020-12-26 22:00:35 +0700

    Update Form.php

--- app/Controllers/Form.php
+++ app/Controllers/Form.php
@@ -3,12 +3,14 @@
 namespace App\Controllers;
 
 use App\Models\AcaraModel;
+use App\Models\KontakModel;
 
 class Form extends BaseController
 {
     public function __construct()
     {
         $this->acaraModel = new AcaraModel();
+        $this->kontakModel = new KontakModel();
     }
     public function index()
     {
@@ -17,5 +19,19 @@ class Form extends BaseController
         echo view('header_v');
         echo view('form/form_v', $data);
         echo view('footer_v');
+    }
+    public function send()
+    {
+        $data = [
+            "namaDepan" => $this->request->getPost('nama-depan'),
+            "namaBelakang" => $this->request->getPost('nama-belakang'),
+            "email" => $this->request->getPost('email'),
+            "nomorTelepon" => $this->request->getPost('nomor-telepon'),
+            // 'acara' => $this->acaraModel->
+        ];
+        // print_r($this->acaraModel);
+        // die;
+        $response = $this->kontakModel->insert($data);
+        return redirect()->to(base_url('acara'));
     }
 }
