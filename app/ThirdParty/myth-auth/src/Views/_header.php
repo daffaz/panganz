3a04d9115335139ee81c1d8ead518319df6ed02d
Daffa Z <dzcoc123@gmail.com>
2021-01-04 16:17:21 +0700

    Update CHANGELOG.md, CONTRIBUTING.md, and 107 more files...

---
+++ app/ThirdParty/myth-auth/src/Views/_header.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Myth:Auth</title>
+
+    <!-- Bootstrap core CSS -->
+    <!-- BOOTSTRAP-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" />
+    <!-- FONTS -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600;700&family=Merriweather:wght@300;700;900&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <!-- FONTAWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+    <!-- CUSTOM CSS -->
+    <link rel="stylesheet" href="<?= base_url('src/css/sb-admin-2.min.css') ?>" />
+</head>
+
+<body>
+
+    <!-- <?php //view('Myth\Auth\Views\_navbar') 
+            ?> -->
+
+    <main role="main" class="container">
---
+++ app/ThirdParty/myth-auth/src/Views/admin_login.php
@@ -0,0 +1,76 @@
+<?= $this->extend($config->viewLayout) ?>
+<?= $this->section('main') ?>
+<section>
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block"><img style="width:480px; height: 383px" src="<?php echo base_url('src/img/logon.jpg') ?>" alt=""></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Selamat datang!</h1>
+                                    </div>
+                                    <?= view('Myth\Auth\Views\_message_block') ?>
+
+                                    <form class="user" action="<?= route_to('login') ?>" method="post">
+                                        <?= csrf_field() ?>
+
+                                        <?php if ($config->validFields === ['email']) : ?>
+                                            <div class="form-group">
+                                                <label for="login"><?= lang('Auth.email') ?></label>
+                                                <input type="email" class="form-control form-control-user <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>" name="login" placeholder="<?= lang('Auth.email') ?>">
+                                                <div class="invalid-feedback">
+                                                    <?= session('errors.login') ?>
+                                                </div>
+                                            </div>
+                                        <?php else : ?>
+                                            <div class="form-group">
+                                                <label for="login"><?= lang('Auth.emailOrUsername') ?></label>
+                                                <input type="text" class="form-control form-control-user <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>" name="login" placeholder="<?= lang('Auth.emailOrUsername') ?>">
+                                                <div class="invalid-feedback">
+                                                    <?= session('errors.login') ?>
+                                                </div>
+                                            </div>
+                                        <?php endif; ?>
+
+                                        <div class="form-group">
+                                            <label for="password"><?= lang('Auth.password') ?></label>
+                                            <input type="password" name="password" class="form-control  <?php if (session('errors.password')) : ?>is-invalid<?php endif ?>" placeholder="<?= lang('Auth.password') ?>">
+                                            <div class="invalid-feedback">
+                                                <?= session('errors.password') ?>
+                                            </div>
+                                        </div>
+
+                                        <?php if ($config->allowRemembering) : ?>
+                                            <div class="form-check">
+                                                <label class="form-check-label">
+                                                    <input type="checkbox" name="remember" class="form-check-input" <?php if (old('remember')) : ?> checked <?php endif ?>>
+                                                    <?= lang('Auth.rememberMe') ?>
+                                                </label>
+                                            </div>
+                                        <?php endif; ?>
+
+                                        <br>
+
+                                        <button type="submit" class="btn btn-info btn-user btn-block"><?= lang('Auth.loginAction') ?></button>
+                                    </form>
+                                    <!-- <hr> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?= $this->endSection() ?>
